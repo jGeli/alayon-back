b8674837968d18e2778a6c3ba468aa81bf32288b
Jaybee <75898150+jaybee-webdev@users.noreply.github.com>
2021-08-17 01:25:26 +0800

    DB Connection

--- app/connection.js
+++ app/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const connection = "mongodb://localhost:27017/mongo-test";
-
+const connectionMongo = "mongodb+srv://aicpa420:aicpa420@cluster0.ljfau.mongodb.net/aicpadb?retryWrites=true&w=majority"
 const connectDb = () => {
-  return mongoose.connect(connection);
+  // return mongoose.connect(connection);
+  return mongoose.connect(connectionMongo);
+
 };
 
 module.exports = connectDb;
